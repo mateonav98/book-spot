4ff8841a7fdb08056e9c1f68cf9c6473ad927b0e
mateonav98 <mateonav98@gmail.com>
2022-05-10 15:40:39 -0700

    fixing get route api

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+const dataRoutes = require('./apiData.js')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/data', dataRoutes);
 
 module.exports = router;
