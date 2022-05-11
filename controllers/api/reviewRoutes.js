eb3c72a9e814ea5bad6b1777dd5123ae2dcf8e74
rich p <richardferry3@gmail.com>
2022-05-10 21:22:29 -0700

    add route to get review by id

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -13,6 +13,17 @@ router.get('/', async (req,res)=>{
         res.status(500).json(err)
     }
 })
-
+// get single route by id
+router.get('/:id', async (req,res) =>{
+    try{
+        const reviewById = await Review.findAll({
+            include: [{model: Book},{model:User}]
+        })
+        const review = reviewById.get({plain: true});
+        res.status(200).json(review);
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
 
 module.exports = router;
