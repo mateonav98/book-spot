f4f5c22f8a49abb71f2ba0627bb13056b88c6fcd
mateonav98 <mateonav98@gmail.com>
2022-05-12 16:28:23 -0700

    fixing conflicts

--- public/js/books.js
+++ public/js/books.js
@@ -1,17 +1,10 @@
+//get book data
 const getData = (event) => {
     event.preventDefault();
     const search = document.querySelector('#searchText').value.trim();
     location.href = '/api/books?searchTerm=' + search;
 
 };
-
-// async function getData(event){
-//     event.preventDefault();
-//     let info = event.target;
-//     const parent = await info.parentNode.parentNode;
-//     console.log(parent)
-// }
-
 document
 .querySelector('.btn')
 .addEventListener('click', getData);
