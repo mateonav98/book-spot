54e5cd58f3ca073b4be65ab226a4b4bd307fd84e
rich p <richardferry3@gmail.com>
2022-05-11 20:12:14 -0700

    move code to new js file

--- public/js/bookresults.js
+++ public/js/bookresults.js
@@ -4,33 +4,7 @@ const getSingleBook = (event) => {
   };
   
 const images = document.querySelectorAll("img");
-console.log('a');
-
 for (const image of images) {
     image.addEventListener('click', getSingleBook);
 }
 
-const commentHandler = async (event) => {
-  event.preventDefault();
-
-  const text = document.querySelector('#reviewText').value.trim();
-  if (text) {
-      const response = await fetch('/api/reviews', {
-          method: 'POST',
-          body: JSON.stringify({review: text}),
-          headers: { 'Content-Type': 'application/json' },
-      });
-      console.log(response)
-      if (response.ok) {
-          // document.location.reload();
-          console.log('SUCCESSFUL')
-      } else {
-          alert('Unable to post')
-      };
-  }
-
-};
-
-document
-  .querySelector('#reviewBtn')
-  .addEventListener('submit', commentHandler);
