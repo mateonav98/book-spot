7a4c4ff71d15505327ac74a1760e943a76f745f3
Kristy Guo <krisxxg@gmail.com>
2022-05-12 17:00:16 -0700

    change topbar thickness to 5 instead of 3

--- public/js/books.js
+++ public/js/books.js
@@ -1,6 +1,22 @@
 const getData = (event) => {
     event.preventDefault();
-    topbar.show();
+ 
+    topbar.config({
+    //     autoRun      : false, 
+        barThickness : 5,
+        // shadowBlur   : 5,
+    //     shadowColor  : 'rgba(0, 0, 0, .5)',
+    //     className    : 'topbar',
+      })
+
+      topbar.show();
+
+    //   (function step() {
+    //     setTimeout(function() {  
+    //       if (topbar.progress('+.01') < 1) step()
+    //     }, 16)
+    //   })()
+
     const search = document.querySelector('#searchText').value.trim();
     location.href = '/api/books?searchTerm=' + search;
 
