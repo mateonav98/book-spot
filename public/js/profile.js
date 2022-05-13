9e97a9cb86c1855bf0dce7e1968e5c3815a56d18
mateonav98 <mateonav98@gmail.com>
2022-05-12 17:41:31 -0700

    adding update and delete buttons

--- public/js/profile.js
+++ public/js/profile.js
@@ -42,50 +42,50 @@ const addReview = async (event) => {
 
 document.querySelectorAll('#submitReview').forEach(e => e.addEventListener('click', addReview));
 
-// updating post to book
-const updateReview = async (event) => {
-    event.preventDefault();
-    let info = await event.target;
-    const id = await info.getAttribute('data');
-    const text = await document.querySelector('#displayText').value.trim();
-    if (text) {
-        const response = await fetch(`/api/reviews/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({review: text),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        console.log(response)
-        if (response.ok) {
-            // document.location.reload();
-            console.log('SUCCESSFUL')
-        } else {
-            alert('Unable to post')
-        };
-    }
-    return document.querySelector('#displayText').innerHTML = text;
-};
+// // updating post to book
+// const updateReview = async (event) => {
+//     event.preventDefault();
+//     let info = await event.target;
+//     const id = await info.getAttribute('data');
+//     const text = await document.querySelector('#displayText').value.trim();
+//     if (text) {
+//         const response = await fetch(`/api/reviews/${id}`, {
+//             method: 'PUT',
+//             body: JSON.stringify({review: text),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         console.log(response)
+//         if (response.ok) {
+//             // document.location.reload();
+//             console.log('SUCCESSFUL')
+//         } else {
+//             alert('Unable to post')
+//         };
+//     }
+//     return document.querySelector('#displayText').innerHTML = text;
+// };
 
 
-// deleting review
-const deleteReview = async (event) => {
-    event.preventDefault();
-    let info = await event.target;
-    const id = await info.getAttribute('data');
-    const response = await fetch(`/api/reviews/${id}`, {
-        method: 'DELETE', 
-    });
-    console.log(response)
-    if (response.ok) {
-        document.location.reload();
-        console.log('UNSAVED SUCCESSFUL')
-    } else {
-        alert('UNSAVE NOT SUCCESSFUL')
-    };
-}
+// // deleting review
+// const deleteReview = async (event) => {
+//     event.preventDefault();
+//     let info = await event.target;
+//     const id = await info.getAttribute('data');
+//     const response = await fetch(`/api/reviews/${id}`, {
+//         method: 'DELETE', 
+//     });
+//     console.log(response)
+//     if (response.ok) {
+//         document.location.reload();
+//         console.log('UNSAVED SUCCESSFUL')
+//     } else {
+//         alert('UNSAVE NOT SUCCESSFUL')
+//     };
+// }
 
 // document
 // .querySelector('#unsaveBtn')
 // .addEventListener('click', unsaveBook);
 
-document.querySelectorAll('#unsaveBtn').forEach(e => e.addEventListener('click', unsaveBook));
+
 
