06c76f54699b2d7fffad69a775d49ae1c879a467
rich p <richardferry3@gmail.com>
2022-05-12 21:19:48 -0700

    join tables through route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ router.get('/profile', withAuth, async (req, res) => {
       // Find the logged in user based on the session ID
       const userData = await User.findByPk(req.session.user_id, {
         attributes: { exclude: ['password'] },
-        include: [{ model: Book},{model:Review}]
+        include: [{ model: Book, include:[{model:Review}]}]
       });
   
       const user = userData.get({ plain: true });
